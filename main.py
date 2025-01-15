aeb449667349b42b84ee4c875638d091d25bd6b8
Glauco <glaucofleury123@gmail.com>
2025-01-15 19:20:03 -0300

    muita coisa

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
-from src.lib import matrix_manipulation as mm
-import numpy as np
+import pandas as pd
+import glob 
+import os
+#OBJETIVO: providenciar dataset, especificações de modelagem, e 
+#terminar com uma plotagem (caso seja 2D/3D) e também com uma
+#capacidade de predição maneira
 
-matrix = [[1, 2, 3, 4], [2, 2, 2, 2], [4, 2, 5, 2]]
-label = [1, 2, 3]
-weight = [1, 1, 1, 1, 1]
+print ("os dados serão buscados na sua pasta de Donwloads")
+user = input("Username: ")
+try:
+    os.chdir(f'/home/{user}/Downloads') 
+except:
+    print('username incorreto')
+    exit()  
+print()
+#tem que digitar o númerozinho
+print("Escolha um pelo número:")
+files = glob.glob('*.csv')
+i = 0
+for file in files:
+    i += 1
+    print(f'{i} = {file}')
+print()
+chosen = input("csv escolhido: ")
+if (int(chosen) > i):
+    print('nah')
+    exit()
+data = pd.read_csv(rf'/home/{user}/Downloads/{files[int(chosen)-1]}')
+#quer grafar oq cm oq? entre 1 e 2 opções
+print()
+num = input('Quantas dimensões você quer usar de Domínio?')
+print('Escolha abaixo suas dimensões, dentre as possíveis:')
+options = list(data.columns)
+i = 0
+for names in options:
+    i+=1
+    print (f'{i}:{names}')
+print()
 
-matrix = np.array(matrix)
-label = np.array(label)
-weight = np.array(weight)
-
-design = mm.Design_Matrix(matrix, 1)
-design = np.array(design)
-print (f"{design}")
-
-print (mm.Gradient_Weights(label, design, 0.1, weight, 0.1))
-# print (mm.Design_Matrix(matrix, 1))
 
 
-# from src.lib import feature_map
 
-# content = input("digite os valores, separando por espaço: ")
-# variaveis = list(map(float, content.split()))#python é um brainrot msm nn é possível
-# print (variaveis)
-# print ("tamanho do vetor: ", len(variaveis))
-# num2 = int (input ("Grau da Expansão: "))
-# print ("resultado: ", feature_map.polynomial_regression (variaveis, len(variaveis), num2))
+
 
